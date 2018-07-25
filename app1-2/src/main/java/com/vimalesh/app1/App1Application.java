0ef957f81ed7d0ec61abfe6a6847ab013876b83c
vimalesh <vimaleshbe@gmail.com>
2018-07-25 13:23:57 +0530

    updated the project

--- app1-2/src/main/java/com/vimalesh/app1/App1Application.java
+++ app1-2/src/main/java/com/vimalesh/app1/App1Application.java
@@ -1,16 +1,16 @@
-package com.vimalesh.zuul;
+package com.vimalesh.app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ZuulApplication {
+@ComponentScan("com.vimalesh")
+public class App1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulApplication.class, args);
+		SpringApplication.run(App1Application.class, args);
 	}
 }
