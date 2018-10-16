513f93cd7a270d0430a78e62292c534b0456689c
vimalesh <vimaleshbe@gmail.com>
2018-10-16 23:13:53 +0530

    added config server logic

--- app1-1/src/main/java/com/vimalesh/controller/AppController.java
+++ app1-1/src/main/java/com/vimalesh/controller/AppController.java
@@ -2,6 +2,7 @@ package com.vimalesh.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,6 @@ public class AppController {
 
 		log.info("test app 1 ");
 
-		return new ResponseEntity<>("hi from app1", HttpStatus.OK);
+		return new ResponseEntity<>("hi from app1 " , HttpStatus.OK);
 	}
 }
--- config-server/src/main/java/com/vimalesh/app1/App1Application.java
+++ config-server/src/main/java/com/vimalesh/app1/App1Application.java
@@ -2,14 +2,17 @@ package com.vimalesh.app1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan("com.vimalesh")
+@EnableConfigServer
 public class App1Application {
 
+	  
 	public static void main(String[] args) {
 		SpringApplication.run(App1Application.class, args);
 	}
